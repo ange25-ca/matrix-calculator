f22a89726ad25a4dd8be93f4180c0d396348770a
AngelicaCahum2525 <monsecahuum@gmail.com>
2024-09-08 23:40:22 -0600

    Implementación de los hijos

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 import MatrixCalculator from './component/calculator';
+import Hecho_en from './component/children';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MatrixCalculator />
+    <Hecho_en>
+      <h5>Made in México</h5>
+    </Hecho_en>
   </React.StrictMode>
 );
